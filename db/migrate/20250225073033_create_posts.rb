4cc97c50ace8e981881320fd0158492c3d9d23e9
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-02-25 20:17:34 +0530

    Initialize new Rails application with default files and configurations

---
+++ db/migrate/20250225073033_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :upvotes, default: 0, null: false
+      t.integer :downvotes, default: 0, null: false
+      t.boolean :is_bloggable, default: false, null: false
+
+      t.timestamps
+    end
+  end
+end
