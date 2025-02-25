64cc6017ca610f2df84bdd5200070764c231e2de
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-02-25 20:17:40 +0530

    Add Post model with validations for title, description, and bloggable status

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ApplicationRecord
+  MAX_TITLE_LENGTH = 125
+  MAX_DESCRIPTION_LENGTH = 10000
+
+  validates :title,
+    presence: true,
+    length: { maximum: MAX_TITLE_LENGTH }
+
+  validates :description,
+    presence: true,
+    length: { maximum: MAX_DESCRIPTION_LENGTH }
+
+  validates :is_bloggable,
+    inclusion: { in: [true, false] }
+
+end
