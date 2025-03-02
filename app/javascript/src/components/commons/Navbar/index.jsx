2c547fb342c32d70e6aeb50a7b3c1b49d1bc3d1a
Suman Sahoo <sumansahoo@Sumans-MacBook-Pro.local>
2025-03-02 15:33:43 +0530

    Add edit button to Navbar for quick post creation navigation

--- app/javascript/src/components/commons/Navbar/index.jsx
+++ app/javascript/src/components/commons/Navbar/index.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 
-import { HamburgerMenu } from "@bigbinary/neeto-icons";
+import { HamburgerMenu, Edit } from "@bigbinary/neeto-icons";
 import { Button, Typography } from "@bigbinary/neetoui";
 import classNames from "classnames";
+import { useHistory } from "react-router-dom";
 
 import { navLinks } from "constants/navbar";
 
 import NavLink from "./Links";
 
 const Navbar = ({ children }) => {
+  const history = useHistory();
+
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
@@ -27,14 +30,24 @@ const Navbar = ({ children }) => {
     "transition-all duration-300 ease-in-out min-h-screen w-full"
   );
 
+  const handleEditClick = () => {
+    history.push("/create");
+  };
+
   return (
     <div className="flex border-r-2 border-black">
-      <div className=" flex min-h-screen items-start border-r-2 border-gray-200 p-2">
+      <div className=" flex min-h-screen flex-col items-start gap-2 border-r-2 border-gray-200 p-2">
         <Button
           className="rounded-md p-2 hover:bg-gray-200"
           icon={HamburgerMenu}
           style="tertiary"
           onClick={toggleSidebar}
+        />
+        <Button
+          className="rounded-md p-2 hover:bg-gray-200"
+          icon={Edit}
+          style="tertiary"
+          onClick={handleEditClick}
         />
       </div>
       <div className={sidebarClasses}>
