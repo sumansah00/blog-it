6433d666b75decf291f3545e9ad9bca4856bbb88
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-03-12 09:44:45 +0530

    Add status enum and update last published timestamp logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   MAX_TITLE_LENGTH = 125
   MAX_DESCRIPTION_LENGTH = 10000
 
+  enum :status, { draft: "draft", published: "published" }, default: :draft
+
   belongs_to :user
   belongs_to :organization
   has_and_belongs_to_many :categories
@@ -11,18 +13,16 @@ class Post < ApplicationRecord
   validates :title,
     presence: true,
     length: { maximum: MAX_TITLE_LENGTH }
-
   validates :description,
     presence: true,
     length: { maximum: MAX_DESCRIPTION_LENGTH }
-
   validates :is_bloggable,
     inclusion: { in: [true, false] }
-
   validates :slug, uniqueness: true
   validate :slug_not_changed
 
   before_create :set_slug
+  before_save :update_last_published_at, if: :status_changed?
 
   private
 
@@ -47,5 +47,13 @@ class Post < ApplicationRecord
       if will_save_change_to_slug? && self.persisted?
         errors.add(:slug, I18n.t("post.slug.immutable"))
       end
+    end
+
+    def update_last_published_at
+      self.last_published_at = Time.current if published? && status_changed_to_published?
+    end
+
+    def status_changed_to_published?
+      status_was != "published" && status == "published"
     end
 end
